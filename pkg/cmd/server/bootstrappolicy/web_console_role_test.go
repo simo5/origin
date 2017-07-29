936db8944de06ed08ca2761a66bb28314a05125c
Simo Sorce <simo@redhat.com>
2017-07-29 16:11:31 -0400

    Fix SystemOnly Annotation for cluster roles
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- pkg/cmd/server/bootstrappolicy/web_console_role_test.go
+++ pkg/cmd/server/bootstrappolicy/web_console_role_test.go
@@ -1,7 +1,6 @@
 package bootstrappolicy
 
 import (
-	"strings"
 	"testing"
 
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -86,12 +85,4 @@ func TestSystemOnlyRoles(t *testing.T) {
 // it is the same logic that is run on the membership page
 func isSystemOnlyRole(role *rbac.ClusterRole) bool {
 	return role.Annotations[roleSystemOnly] == roleIsSystemOnly
-}
-
-// helper so that roles following this pattern do not need to be manaully added
-// to the hide list
-func isControllerRole(role *rbac.ClusterRole) bool {
-	return strings.HasPrefix(role.Name, "system:controller:") ||
-		strings.HasSuffix(role.Name, "-controller") ||
-		strings.HasPrefix(role.Name, "system:openshift:controller:")
 }
